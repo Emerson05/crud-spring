f8b0ac3bbdde74080e30988990c7f38abd448129
Emerson <emersonjuniormt@gmail.com>
2024-07-24 19:29:54 -0300

    Atualizando rotas api

--- src/main/java/br/com/projeto/api/controller/Controller.java
+++ src/main/java/br/com/projeto/api/controller/Controller.java
@@ -6,10 +6,16 @@ import br.com.projeto.api.models.Pessoa;
 import br.com.projeto.api.repository.Repositorio;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import java.util.List;
+
 
 
 
@@ -19,6 +25,77 @@ public class Controller {
 
     @Autowired
     private Repositorio acao;
+
+    @GetMapping("/api")
+
+    public List<Pessoa> mostrar(){
+        return acao.findAll();
+    }
+    
+    @GetMapping("/api/ordenanomes")
+
+    public List<Pessoa> ordernaNome(){
+        return acao.findByOrderByNome();
+    }
+
+    @GetMapping("api/ordernanomesiguais")
+    public List <Pessoa> ordernaNomesIguais(){
+        return acao.findByNomeOrderByIdadeDesc("Jorge");
+    }
+
+    @GetMapping("api/somaidades")
+    public int somaIdades(){
+        return acao.somaIdades();
+    }
+
+    @GetMapping("api/nomeconteudo")
+
+    public List <Pessoa> nomeConteudoEspecifico(){
+        return acao.findByNomeContaining("E");
+    }
+
+    @GetMapping("/api/iniciacom")
+    public List<Pessoa> iniciaCom(){
+        return acao.findByNomeStartsWith("j");
+    }
+    @GetMapping("/api/terminacom")
+    public List<Pessoa> terminaCom(){
+        return acao.findByNomeEndsWith("a");
+    }
+
+    @GetMapping("/api/{codigo}")
+
+    public Pessoa acharCodigo(@PathVariable int codigo){
+        return acao.findBycodigo(codigo);
+    }
+    
+    @PostMapping("/api")
+    public Pessoa cadastrar(@RequestBody Pessoa obj){
+        return acao.save(obj);
+    }
+
+    @PutMapping("/api")
+
+    public Pessoa editar(@RequestBody Pessoa obj){
+        return acao.save(obj);
+    }
+
+    @DeleteMapping("/api/{codigo}")
+
+    public void remover(@PathVariable int codigo){
+        Pessoa obj = acao.findBycodigo(codigo);
+
+        acao.delete(obj);
+
+    }
+
+    @GetMapping("api/contador")
+
+    public long contador(){
+        return acao.count();
+
+    }
+
     @GetMapping("")
     public String mensagem() {
         return "Hellow World";
@@ -41,6 +118,7 @@ public class Controller {
         return entity;
     }
     
+    @GetMapping("/pessoa")
     public Pessoa pessoa(@RequestBody Pessoa p ){
         return p;
     }
--- src/main/java/br/com/projeto/api/repository/Repositorio.java
+++ src/main/java/br/com/projeto/api/repository/Repositorio.java
@@ -1,10 +1,28 @@
 package br.com.projeto.api.repository;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import br.com.projeto.api.models.Pessoa;
+import java.util.List;
 
 @Repository
 public interface Repositorio extends CrudRepository<Pessoa, Integer> {
-    
+    List <Pessoa> findAll();
+
+    Pessoa findBycodigo(int codigo);
+
+    List<Pessoa> findByOrderByNome();
+
+    List<Pessoa> findByNomeOrderByIdadeDesc(String nome);
+
+    List<Pessoa> findByNomeContaining(String termo);
+
+    List<Pessoa> findByNomeStartsWith(String termo);
+
+    List<Pessoa> findByNomeEndsWith(String termo);
+
+    @Query(value = "SELECT SUM(idade) FROM pessoas", nativeQuery = true);
+    int somaIdades();
+
 }
